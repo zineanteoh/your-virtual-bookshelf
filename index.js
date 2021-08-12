0527afe70fc794d8cff304d90c8143ee22aaf388
Zi Nean Teoh <zinean00@gmail.com>
2021-08-12 22:16:59 +0800

    Implemented popup window to add new book to shelf

--- index.js
+++ index.js
@@ -1,52 +1,76 @@
-const catalog = document.querySelector(".catalog");
-
 let myLibrary = [];
+
+const catalog = document.querySelector(".catalog");
+const popup = document.querySelector(".add-button");
+const overlay = document.querySelector(".overlay");
+
+overlay.addEventListener("click", togglePopup);
+popup.addEventListener("click", togglePopup);
+document.addEventListener("keydown", (event) => {
+  if (!overlay.classList.contains("hidden")) {
+    if (event.key == "Escape") {
+      togglePopup();
+    }
+  }
+});
+document.querySelector(".popup-form").addEventListener("submit", (event) => {
+  event.preventDefault();
+});
 
 function Book(title, author, pages, isRead) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.isRead = isRead;
-  this.info = info;
-  function info() {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${
-      this.isRead ? "read" : "not read yet"
-    }`;
-  }
-}
-
-function addBookToLibrary(book) {
-  myLibrary.push(book);
 }
 
 function displayLibrary() {
   myLibrary.forEach(function (book) {
-    let newBook = document.createElement("div");
-    let title = document.createElement("h3");
-    let author = document.createElement("h3");
-    let pages = document.createElement("h3");
-
-    newBook.classList.add("book");
-    title.textContent = `"${book.title}"`;
-    author.textContent = book.author;
-    pages.textContent = book.pages;
-
-    newBook.appendChild(title);
-    newBook.appendChild(author);
-    newBook.appendChild(pages);
-    catalog.appendChild(newBook);
+    catalog.appendChild(createBookElement(book));
   });
 }
 
-function toggleAddNewBook() {}
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+  catalog.appendChild(createBookElement(book));
+}
 
-// Temporary code: manually adding some books
-addBookToLibrary(
-  new Book("21 Lessons for the 21st Century", "Yuval Noah Harari", 300, true)
-);
-addBookToLibrary(new Book("Sapiens", "Yuval Noah Harari", 300, true));
-addBookToLibrary(new Book("Factfulness", "Hans Rosling", 300, true));
+function processBook() {
+  let title = document.querySelector("#title").value;
+  let author = document.querySelector("#author").value;
+  let pages = document.querySelector("#pages").value;
+  let isRead = document.querySelector("#status").checked;
+  addBookToLibrary(new Book(title, author, pages, isRead));
+  clearWindow();
+  togglePopup();
+}
 
-window.onload = () => {
-  displayLibrary();
-};
+function createBookElement(book) {
+  let newBook = document.createElement("div");
+  newBook.classList.add("book");
+
+  let title = document.createElement("h3");
+  let author = document.createElement("h3");
+  let pages = document.createElement("h3");
+
+  title.textContent = `"${book.title}"`;
+  author.textContent = book.author;
+  pages.textContent = book.pages;
+
+  newBook.appendChild(title);
+  newBook.appendChild(author);
+  newBook.appendChild(pages);
+  return newBook;
+}
+
+function clearWindow() {
+  document.querySelector("#title").value = "";
+  document.querySelector("#author").value = "";
+  document.querySelector("#pages").value = "";
+  document.querySelector("#status").checked = false;
+}
+
+function togglePopup() {
+  document.querySelector(".overlay").classList.toggle("hidden");
+  document.querySelector(".popup").classList.toggle("hidden");
+}
