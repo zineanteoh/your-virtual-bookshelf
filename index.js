0f726120a7eab779be039c8b337901a2f8403162
Zi Nean Teoh <zinean00@gmail.com>
2021-08-12 19:52:34 +0800

    Added add-book button

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-let myLibrary = [];
+const catalog = document.querySelector(".catalog");
 
-// Temporary code: manually adding some books
-myLibrary.push(
-  new Book("21 Lessons for the 21st Century", "Yuval Noah Harari", 300, true)
-);
-myLibrary.push(new Book("Sapiens", "Yuval Noah Harari", 300, true));
-myLibrary.push(new Book("Factfulness", "Hans Rosling", 300, true));
+let myLibrary = [];
 
 function Book(title, author, pages, isRead) {
   this.title = title;
@@ -20,6 +15,38 @@ function Book(title, author, pages, isRead) {
   }
 }
 
-function addBookToLibrary(book) {}
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
 
-function displayLibrary() {}
+function displayLibrary() {
+  myLibrary.forEach(function (book) {
+    let newBook = document.createElement("div");
+    let title = document.createElement("h3");
+    let author = document.createElement("h3");
+    let pages = document.createElement("h3");
+
+    newBook.classList.add("book");
+    title.textContent = `"${book.title}"`;
+    author.textContent = book.author;
+    pages.textContent = book.pages;
+
+    newBook.appendChild(title);
+    newBook.appendChild(author);
+    newBook.appendChild(pages);
+    catalog.appendChild(newBook);
+  });
+}
+
+function toggleAddNewBook() {}
+
+// Temporary code: manually adding some books
+addBookToLibrary(
+  new Book("21 Lessons for the 21st Century", "Yuval Noah Harari", 300, true)
+);
+addBookToLibrary(new Book("Sapiens", "Yuval Noah Harari", 300, true));
+addBookToLibrary(new Book("Factfulness", "Hans Rosling", 300, true));
+
+window.onload = () => {
+  displayLibrary();
+};
